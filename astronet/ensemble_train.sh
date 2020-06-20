232c8434206bb825e86cc92ed87687432cff92a1
Dan Moldovan <mdan@google.com>
2020-06-20 19:13:49 +0000

    Update run number.

--- astronet/ensemble_train.sh
+++ astronet/ensemble_train.sh
@@ -6,9 +6,9 @@ do
     python astronet/train.py \
         --model=AstroCNNModel \
         --config_name=local_global_new_tuned \
-        --train_files='astronet/tfrecords-11/test-0000[0-5]*' \
-        --eval_files='astronet/tfrecords-11/test-0000[6-6]*' \
+        --train_files='/mnt/tess/astronet/tfrecords-11/test-0000[0-5]*' \
+        --eval_files='/mnt/tess/astronet/tfrecords-11/test-0000[6-6]*' \
         --train_steps=20000 \
-        --model_dir="astronet/checkpoints/local_global_new_11/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/local_global_new_11/${i}"
 done
 
