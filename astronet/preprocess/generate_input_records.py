Fix normalization code to guarantee values between (-1, 1). Add view of sample periods with mask.

astronet/preprocess/generate_input_records.py
@@ -130,14 +130,15 @@ parser.add_argument(
 def _set_float_feature(ex, name, value):
   """Sets the value of a float feature in a tensorflow.train.Example proto."""
   assert name not in ex.features.feature, "Duplicate feature: %s" % name
+  if isinstance(value, np.ndarray):
+    value = value.reshape((-1,))
   ex.features.feature[name].float_list.value.extend([float(v) for v in value])
 
 
 def _set_bytes_feature(ex, name, value):
   """Sets the value of a bytes feature in a tensorflow.train.Example proto."""
   assert name not in ex.features.feature, "Duplicate feature: %s" % name
-  ex.features.feature[name].bytes_list.value.extend([
-      str(v).encode("latin-1") for v in value])
+  ex.features.feature[name].bytes_list.value.extend([str(v).encode("latin-1") for v in value])
 
 
 def _set_int64_feature(ex, name, value):
@@ -155,21 +156,26 @@ def _process_tce(tce, use_old_detrending=False, bkspace_min=0.5):
         tce.tic_id, time, flux, tce.Period, tce.Epoc, tce.Duration, bkspace_min)
   time, flux, fold_num = preprocess.phase_fold_and_sort_light_curve(time, flux, tce.Period, tce.Epoc)
 
-  global_view = preprocess.global_view(tce.tic_id, time, flux, tce.Period)
-  local_view = preprocess.local_view(tce.tic_id, time, flux, tce.Period, tce.Duration)
-  secondary_view = preprocess.secondary_view(tce.tic_id, time, flux, tce.Period, tce.Duration)
+  # TODO: Include the mask in the data.
+  global_view, _ = preprocess.global_view(tce.tic_id, time, flux, tce.Period)
+  local_view, _ = preprocess.local_view(tce.tic_id, time, flux, tce.Period, tce.Duration)
+  secondary_view, _ = preprocess.secondary_view(tce.tic_id, time, flux, tce.Period, tce.Duration)
+  sampled_view = preprocess.sampled_view(tce.tic_id, time, flux, fold_num, tce.Period)
   if any(np.isnan(global_view)):
     raise ValueError("NaN in global view for {}".format(tce.tic_id))
   if any(np.isnan(local_view)):
     raise ValueError("NaN in local view for {}".format(tce.tic_id))
   if any(np.isnan(secondary_view)):
     raise ValueError("NaN in secondary view for {}".format(tce.tic_id))
+  if any(np.isnan(sampled_view).reshape((-1,))):
+    raise ValueError("NaN in sampled view for {}".format(tce.tic_id))
 
   ex = tf.train.Example()
 
   _set_float_feature(ex, 'global_view', global_view)
   _set_float_feature(ex, 'local_view', local_view)
   _set_float_feature(ex, 'secondary_view', secondary_view)
+  _set_float_feature(ex, 'sampled_view', sampled_view)
   _set_float_feature(ex, 'n_folds', [max(fold_num)])
   _set_float_feature(ex, 'n_points', [len(fold_num)])
     

astronet/preprocess/preprocess.py
@@ -90,24 +90,17 @@ def phase_fold_and_sort_light_curve(time, flux, period, t0):
   return time, flux, fold_num
 
 
-def select_transits(time, flux, fold_num, period, num_transits=10):
-    n_folds = max(fold_num) + 1
-    fold_size = [np.count_nonzero(fold_num == i) for i in range(n_folds)]
-    # Add a small amount of noise to break ties between equally sized folds.
-    sort_indicator = [fs + np.random.uniform(0.5) for fs in fold_size]
-    sorted_fold_num = np.flip(np.argsort(sort_indicator))
-    fold_nums = sorted_fold_num[:num_transits]
-
-    times = []
-    fluxes = []
-    for i in fold_nums:
-        times.append(time[fold_num == i])
-        fluxes.append(flux[fold_num == i])
-    return times, fluxes, fold_nums
-
-
-def generate_view(tic_id, time, flux, period, num_bins, bin_width, t_min, t_max,
-                  normalize=True, new_binning=True):
+def generate_view(tic_id,
+                  time,
+                  flux,
+                  period,
+                  num_bins,
+                  bin_width,
+                  t_min,
+                  t_max,
+                  normalize=True,
+                  new_binning=True,
+                 ):
   """Generates a view of a phase-folded light curve using a median filter.
 
   Args:
@@ -125,22 +118,24 @@ def generate_view(tic_id, time, flux, period, num_bins, bin_width, t_min, t_max,
   """
   try:
     if new_binning:
-      view = median_filter2.new_binning(time, flux, period, num_bins, t_min, t_max)
+      view, mask = median_filter2.new_binning(time, flux, period, num_bins, t_min, t_max)
     else:
       view = median_filter.median_filter(time, flux, num_bins, bin_width, t_min, t_max)
+      mask = np.ones_like(view)
   except:
     logging.warning("Robust mean failed for %s, using median", tic_id)
     view = median_filter.median_filter(time, flux, num_bins, bin_width, t_min, t_max)
+    mask = np.ones_like(view)
 
   if normalize:
-    view -= np.median(view)
-    scale = np.abs(np.min(view))
-    # In pathological cases, min(view) is zero...
-    scale = np.where(scale != 0, scale, np.ones_like(scale))
-    view /= scale
-    
+    bool_mask = mask > 0
+    view = np.where(bool_mask, view - np.min(view[bool_mask]), view)
+    scale = np.abs(np.max(view) / 2)
+    if scale > 0:
+        view /= scale
+    view -= 1.0
 
-  return view
+  return view, mask
 
 
 def global_view(tic_id, time, flux, period, num_bins=201, bin_width_factor=1.2/201, new_binning=True):
@@ -311,7 +306,7 @@ def secondary_view(tic_id,
 
     return generate_view(
         tic_id, 
-      new_time,
+        new_time,
         new_flux,
         period,
         num_bins=num_bins,
@@ -320,3 +315,50 @@ def secondary_view(tic_id,
         t_max=t_max
     )
 
+
+def sample_transits(time, flux, fold_num, period, num_transits=10):
+    n_folds = max(fold_num) + 1
+    fold_size = [np.count_nonzero(fold_num == i) for i in range(n_folds)]
+    # Add a small amount of noise to break ties between equally sized folds.
+    sort_indicator = [fs + np.random.uniform(0.5) for fs in fold_size]
+    sorted_fold_num = np.flip(np.argsort(sort_indicator))
+    fold_nums = sorted_fold_num[:num_transits]
+
+    times = []
+    fluxes = []
+    for i in fold_nums:
+        times.append(time[fold_num == i])
+        fluxes.append(flux[fold_num == i])
+    return times, fluxes, fold_nums
+
+
+
+def sampled_view(tic_id, 
+                 time,
+                 flux,
+                 fold_num,
+                 period,
+                 num_bins=201,
+                 bin_width_factor=1.2 / 201,
+                 num_transits=7,
+                ):
+    times, fluxes, nums = sample_transits(time, flux, fold_num, period, num_transits=num_transits)
+    full_view = []
+    for t, f in zip(times, fluxes):
+        full_view.extend(
+            generate_view(
+                tic_id, 
+                t,
+                f,
+                period,
+                num_bins=num_bins,
+                bin_width=period * bin_width_factor,
+                t_min=min(t),
+                t_max=max(t),
+            )
+        )
+
+    # values in channel i, mask in channel i + 1
+    zipped = np.array(list(zip(*full_view)))
+    return zipped
+        