d8cab286e4d434d54e2eb6654c2e6fa8ebcc8aa1
Dan Moldovan <mdan@google.com>
2020-11-15 15:52:44 +0000

    Add new preprocessing script.

---
+++ astronet/gen_tfrecords.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+TEMPDIR=tmp/tfrecords
+LCDIR=~/lc
+
+rm -Rf ${TEMPDIR}
+mkdir -p ${TEMPDIR}
+
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v3-train.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-14-train --num_shards=20
+
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v3-val.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-14-val --num_shards=2
+
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v3-test.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-14-test --num_shards=2
+
+cp -R ${TEMPDIR}/* /mnt/tess/astronet
+rm -Rf tmp
