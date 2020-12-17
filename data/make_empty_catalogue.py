72bf9888de4a8e65b8493411d56bd57a25fafc2d
mkunimoto <33587249+mkunimoto@users.noreply.github.com>
2020-12-17 08:23:30 -0800

    Update make_empty_catalogue.py

--- data/make_empty_catalogue.py
+++ data/make_empty_catalogue.py
@@ -1,57 +1,29 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import os
-
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "--base_dir",
-    type=str,
-    default='/pdo/users/mkuni',
-    help="Directory where TCE lists are located.")
-
-parser.add_argument(
-    '--input',
-    type=str,
-    help='txt file containing the TIC IDs of TCEs that will go into CSV table.',
-    required=True)
-
-parser.add_argument(
-    '--output',
-    type=str,
-    help='name of output CSV table',
-    required=True)
-
-parser.add_argument(
-    "--save_dir",
-    type=str,
-    default='/pdo/users/mkuni',
-    help="Directory where CSV file will be generated.")
-
-parser.add_argument(
-    "--sector",
-    type=int,
-    help='sector number')
-
-parser.add_argument(
-    "--cam",
-    type=int,
-    help="camera number")
-
-parser.add_argument(
-    "--ccd",
-    type=int,
-    help="ccd number")
+parser.add_argument('--base_dir', type=str, help='Directory where TCE lists are located', required=True)
+parser.add_argument('--input', type=str, help='txt file containing the TIC IDs of TCEs that will go into CSV table', required=True)
+parser.add_argument('--save_dir', type=str, help='Directory where CSV file will be generated', required=True)
+parser.add_argument('--sector', type=int, default=1, help='the sector for which this catalog is generating')
+parser.add_argument('--cam', type=int, default=1, help='the cam for which this catalog is generating')
+parser.add_argument('--ccd', type=int, default=1, help='the ccd for which this catalog is generating')
+parser.add_argument('--output', help='the name of the output CSV table')
 
 FLAGS, unparsed = parser.parse_known_args()
 
-columns = ['tic_id','Tmag','Period','Epoc','Transit_Depth','Duration','Sectors','Camera','CCD']
-tics = np.loadtxt(FLAGS.input, dtype=int)
-lc_tces = pd.DataFrame(columns=columns)
-lc_tces['tic_id'] = tics
-lc_tces['Sectors'] = FLAGS.sector
-lc_tces['Camera'] = FLAGS.cam
-lc_tces['CCD'] = FLAGS.ccd
-lc_tces.to_csv(os.path.join(FLAGS.save_dir, FLAGS.output),index=False)
+columns = ['tic_id', 'RA', 'Dec', 'Tmag', 'Period', 'Epoc', 'Transit_Depth', 'Duration', 'Sectors', 'Camera', 'CCD']
+tics = np.loadtxt(os.path.join(FLAGS.base_dir, FLAGS.input), dtype=int)
+tces = pd.DataFrame(columns=columns)
+tces['tic_id'] = tics
+tces['Sectors'] = FLAGS.sector
+tces['Camera'] = FLAGS.cam
+tces['CCD'] = FLAGS.ccd
+if FLAGS.output is not None:
+    fname = FLAGS.output
+else:
+    fname = 's%i_cam%i_ccd%i.csv' % (FLAGS.sector, FLAGS.cam, FLAGS.ccd)
+tces.to_csv(os.path.join(FLAGS.save_dir, fname), index=False)
